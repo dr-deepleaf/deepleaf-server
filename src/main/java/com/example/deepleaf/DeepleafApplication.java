3c33f6c6c2951710a5ec0794fcd7a6cf8e28ad89
gdrffg <gdrffg@gmail.com>
2025-07-01 21:09:59 +0900

    init

---
+++ src/main/java/com/example/deepleaf/DeepleafApplication.java
@@ -0,0 +1,13 @@
+package com.example.deepleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeepleafApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeepleafApplication.class, args);
+    }
+
+}
