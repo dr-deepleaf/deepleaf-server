6bba62d336b737102e870fb76e32e8dcbc1f1aa1
gdrffg <gdrffg@gmail.com>
2025-07-17 23:11:50 +0900

    feat: 로그인 인터셉터

---
+++ src/main/java/com/example/deepleaf/global/config/WebMvcConfig.java
@@ -0,0 +1,22 @@
+package com.example.deepleaf.global.config;
+
+
+import com.example.deepleaf.auth.interceptor.LoginCheckInterceptor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@RequiredArgsConstructor
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+    private final LoginCheckInterceptor loginCheckInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loginCheckInterceptor)
+                .order(1)
+                .addPathPatterns("/api/**")
+                .excludePathPatterns("/auth/**");
+    }
+}
