7e48832f14c4aa3f1c42d3592b15b6515aee51aa
gdrffg <gdrffg@gmail.com>
2026-01-29 16:24:17 +0900

    refactor: configs

--- src/main/java/com/example/deepleaf/global/config/redis/RedisConfig.java
+++ src/main/java/com/example/deepleaf/global/config/redis/RedisConfig.java
@@ -3,22 +3,29 @@ package com.example.deepleaf.global.config.redis;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisPassword;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.util.StringUtils;
 
 @Configuration
 public class RedisConfig {
     @Value("${spring.data.redis.host}")
     private String host;
-  
+
     @Value("${spring.data.redis.port}")
     private int port;
-  
+
+    @Value("${spring.data.redis.password:}")
+    private String password;
+
     @Bean
     public LettuceConnectionFactory redisConnectionFactory() {
-      // Lettuce라는 라이브러리를 활용해 Redis 연결을 관리하는 객체를 생성하고
-      // Redis 서버에 대한 정보(host, port)를 설정한다. 
-      return new LettuceConnectionFactory(new RedisStandaloneConfiguration(host, port));
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration(host, port);
+        if (StringUtils.hasText(password)) {
+            config.setPassword(RedisPassword.of(password));
+        }
+        return new LettuceConnectionFactory(config);
     }
    
 }
